7d6b4320a1f4b47c82beca47f543cee3a35d2291
JoãoMessias <94173968+JoaoMessiass07@users.noreply.github.com>
2024-04-08 09:50:50 -0300

    laço for

---
+++ Python/for/laco_for_exemplo.py
@@ -0,0 +1,15 @@
+#Exemplo de aplicacao em laço
+import os
+os.system("cls")
+
+#valor inicial, valor final (até um número antes) e incremento
+#(1 - valor inicial, 10 - valor final( até um número antes), 1 - incremento (significa o que ele vai fazer))
+num = int(input("Número: "))
+for volta in range(num, num * 10 + 1 , num):
+    print(f"{volta} ", end= " ")
+
+print()
+
+for volta in range(num, 11, 1):
+    tab = num * volta
+    print(f"{volta} ", end= " ")
