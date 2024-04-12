087a59657d4d5e3a1b99babcf9b99a71aacdc6ad
JoãoMessias <94173968+JoaoMessiass07@users.noreply.github.com>
2024-04-12 11:48:08 -0300

    update

--- Python/for/for_ex/ex3.py
+++ Python/for/for_ex/ex3.py
@@ -4,27 +4,22 @@ os.system("cls")
 
 #Peça dois números ao usuário e a ordem ("C"rescente ou "D"decrescente) e exiba o intervalo devido.
 
-n1 = int(input("N° 1: ")) #5
-n2 = int(input("N° 2: ")) #8
+num1 = int(input("N° 1: ")) #5
+num2 = int(input("N° 2: ")) #8
 ordem = str(input("<C>rescente ou <D>ecrescente: "))
 
-if ordem == 'C' or ordem == 'c':
-    if( n1 == n2 ):
-        print("Números digitados são iguais!")
-    else: 
-        for cresc in range(n1, n2 + 1, 1):
-            print(f"{cresc} ", end= " ")
+if num1 < num2:
+    menor = num1
+    maior = num2
+else:
+    menor = num2
+    maior = num1
 
-        for cresc in range(n2, n1 + 1, 1):
-            print(f"{cresc} ", end= " ")
-elif ordem == 'D' or ordem == 'd':
-    if( n1 == n2 ):
-        print("Números digitados são iguais!")
-    else: 
-        for decresc in range(n1, n2-1, -1):
-            print(f"{decresc} ", end= " ")
-        
-        for decresc in range(n2, n1-1, -1):
-            print(f"{decresc} ", end= " ")
+if ordem == 'c' or ordem == 'C':
+    for i in range(menor, maior + 1, 1):
+        print(i, end = ' ')
+elif ordem.upper() == "D":
+    for i in range(maior, menor - 1, -1):
+        print(i, end = ' ')
 else:
     print("Opção inválida!!")
