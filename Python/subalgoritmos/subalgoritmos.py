2a4b227c6e738400fbcfc929ba193ae766ebed56
JoãoMessias <94173968+JoaoMessiass07@users.noreply.github.com>
2024-04-22 10:20:21 -0300

    att

--- Python/subalgoritmos/subalgoritmos.py
+++ Python/subalgoritmos/subalgoritmos.py
@@ -1,8 +1,24 @@
-# Calcular a equação do 2.0 grau por Baskara
-# SUBALGORITMOS - FUNÇÃO E PROCEDIMENTOS ===> MÉTODOS
-import os
-os.system("cls")
-
-# ============== SUBALGORITMOS
-# ---- Procedimento ==> Subalgoritmos que não retorna valor ao programa chamador
-
+# ---- Procedimento ==> Subalgoritmo que não retorna valor ao programa chamador
+# ==> Sem passagem de parâmetros
+def saudacao1():
+    print("Bom dia, usuário")
+ 
+def saudacao2(nome):
+    print(f"Bom dia, {nome}")
+ 
+def saudacao3(nome, hora):
+    if hora < 12:
+        msg = "Bom dia"
+    elif hora < 18:
+        msg = "Boa tarde"
+    else:
+        msg = "Boa noite"
+    print(f"{msg}, {nome}")
+ 
+# ---- Função ==> Subalgoritmo que retorna valor ao programa chamador
+ 
+ 
+# ======================== PROGRAMA PRINCIPAL
+saudacao1()
+saudacao2("Maria")
+saudacao3("Fernanda", 19)
