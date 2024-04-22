cbad701ff549b0d1172f1c76ed429106759f4e43
JoãoMessias <94173968+JoaoMessiass07@users.noreply.github.com>
2024-04-22 08:38:25 -0300

    att

---
+++ Python/subalgoritmos/subalgoritmos.py
@@ -0,0 +1,8 @@
+# Calcular a equação do 2.0 grau por Baskara
+# SUBALGORITMOS - FUNÇÃO E PROCEDIMENTOS ===> MÉTODOS
+import os
+os.system("cls")
+
+# ============== SUBALGORITMOS
+# ---- Procedimento ==> Subalgoritmos que não retorna valor ao programa chamador
+
