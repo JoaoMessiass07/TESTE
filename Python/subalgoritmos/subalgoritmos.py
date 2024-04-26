13835baec8949998a762d3419ec5f4c21eae3bc1
JoãoMessias <94173968+JoaoMessiass07@users.noreply.github.com>
2024-04-26 01:08:08 -0300

    att

--- Python/subalgoritmos/subalgoritmos.py
+++ Python/subalgoritmos/subalgoritmos.py
@@ -1,24 +1,50 @@
-# ---- Procedimento ==> Subalgoritmo que não retorna valor ao programa chamador
-# ==> Sem passagem de parâmetros
-def saudacao1():
-    print("Bom dia, usuário")
- 
-def saudacao2(nome):
-    print(f"Bom dia, {nome}")
- 
-def saudacao3(nome, hora):
-    if hora < 12:
-        msg = "Bom dia"
-    elif hora < 18:
-        msg = "Boa tarde"
-    else:
-        msg = "Boa noite"
-    print(f"{msg}, {nome}")
- 
-# ---- Função ==> Subalgoritmo que retorna valor ao programa chamador
- 
- 
+# Calcular a equação do 2.o grau por Baskara
+# SUBALGORITMOS - FUNÇÕES E PROCEDIMENTOS ====> MÉTODOS
+from sub import * # * ==> todos
+import os
+os.system("clear")
+
 # ======================== PROGRAMA PRINCIPAL
+from sub import saudacao1, saudacao2, saudacao3, raiz2, raizn, pi
 saudacao1()
 saudacao2("Maria")
-saudacao3("Fernanda", 19)
+saudacao3("Fernanda", 17)
+
+
+print(raiz2(16))
+print(raizn(27, 5))
+print(pi())
+r = 2
+c = 2 * pi() * r
+print(r, c)
+
+
+"""
+from sub import saudacao1, saudacao2, saudacao3, raiz2, raizn, pi
+saudacao1()
+saudacao2("Maria")
+saudacao3("Fernanda", 17)
+
+
+print(raiz2(16))
+print(raizn(27, 5))
+print(pi())
+r = 2
+c = 2 * pi() * r
+print(r, c)
+"""
+"""
+import sub
+
+sub.saudacao1()
+sub.saudacao2("Maria")
+sub.saudacao3("Fernanda", 17)
+
+
+print(sub.raiz2(16))
+print(sub.raizn(27, 5))
+print(sub.pi())
+r = 2
+c = 2 * sub.pi() * r
+print(r, c)
+"""
